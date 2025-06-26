d1c7bd2c7ee24102f00e07d42a099247c2b8c16c
Leoka2000 <reusoliveiraleo@gmail.com>
2025-06-27 01:18:39 +0200

    sb

--- my-app/resources/views/dashboard.blade.php
+++ my-app/resources/views/dashboard.blade.php
@@ -13,6 +13,8 @@
                 class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern
                     class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                <livewire:toggle_btn />
+
             </div>
             <div
                 class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">

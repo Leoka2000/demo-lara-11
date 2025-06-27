d53463f6f6f0b75ee840d054e0816611704a43d9
Leoka2000 <reusoliveiraleo@gmail.com>
2025-06-27 03:01:23 +0200

    apexchart

--- my-app/resources/views/components/layouts/app/sidebar.blade.php
+++ my-app/resources/views/components/layouts/app/sidebar.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     @include('partials.head')
+
 </head>
 
 <body class="min-h-screen bg-white dark:bg-zinc-800">
@@ -125,6 +126,7 @@
     {{ $slot }}
 
     @fluxScripts
+
 </body>
 
 </html>
--- my-app/resources/views/partials/head.blade.php
+++ my-app/resources/views/partials/head.blade.php
@@ -8,8 +8,15 @@
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
+
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<!-- ApexCharts CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@latest/dist/apexcharts.min.css">
+<!-- ApexCharts JS -->
+<script src="https://cdn.jsdelivr.net/npm/apexcharts@latest/dist/apexcharts.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
